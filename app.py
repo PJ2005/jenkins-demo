db2e19f131b5a24de6e22f1f2e180ae55451d929
PJ2005 <93432926+PJ2005@users.noreply.github.com>
2026-08-25 10:25:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello from Jenkins!"
+def add(a, b):
+    return a + b
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    print("Result:", add(2, 3))
